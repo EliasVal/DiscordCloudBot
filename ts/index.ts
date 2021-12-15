b51f78866f72c094b7cc22f90ae48d35312875c4
EliasV_ <46247153+EliasVal@users.noreply.github.com>
2021-12-15 18:16:21 +0200

    Basic Bot Stuff

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,16 @@
-console.log("HELLO");
+import * as discord from "discord.js";
+import { config } from "dotenv";
+
+config();
+
+const client = new discord.Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "DIRECT_MESSAGES",
+    "GUILD_MEMBERS",
+    "GUILD_MESSAGE_REACTIONS",
+  ],
+});
+
+client.login(process.env.TOKEN);
